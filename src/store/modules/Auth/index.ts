9e63206af8750aea55bc1fc637018d344d154907
Callan <cerveauxallanjean@gmail.com>
2019-01-09 11:32:19 +0100

    Login & Register work

--- src/store/modules/Auth/index.ts
+++ src/store/modules/Auth/index.ts
@@ -1,6 +1,7 @@
 import {GetterTree, MutationTree, ActionTree, ActionContext} from "vuex";
 import {State} from "./types";
 // Graphql module
+import {login, me, register} from "./schema";
 // @ts-ignore
 import { apolloClient } from "@/ApolloClient.ts"
 // @ts-ignore
@@ -12,14 +13,42 @@ const state: State = {
 };
 
 const getters: GetterTree<State, any> = {
+    getUser: (state) => state.user
 };
 
 const mutations: MutationTree<State> = {
-
+    mutateLogin(state, payload){
+        apolloClient.mutate({
+            mutation: login,
+            variables:{
+                email:payload.email,
+                password: payload.password
+            }
+        })
+            .then(res => {
+                localStorage.setItem(LS_AUTH_TOKEN, res.data.login);
+            })
+            .catch(err => err ? console.log(err) : "")
+    }
 };
 
 const actions: ActionTree<State, any> = {
-
+    LOGIN({commit}, payload){
+        commit("mutateLogin", payload);
+    },
+    REGISTER({commit}, payload){
+        if(payload.password === payload.passwordRepeat){
+            apolloClient.mutate({
+                mutation: register,
+                variables:{
+                    email: payload.email,
+                    password: payload.password
+                }
+            })
+                .then(res => console.log(res))
+                .catch(err => err ? console.log(err) : "")
+        }
+    }
 };
 
 export const auth = {
--- src/store/modules/Auth/schema.ts
+++ src/store/modules/Auth/schema.ts
@@ -1,1 +1,23 @@
 import gql from 'graphql-tag';
+
+export const login = gql`    
+    mutation ($email: String!, $password: String!){
+        login(email: $email, password: $password)
+    }
+`;
+
+export const register = gql`
+  mutation ($email: String!, $password: String!){
+      userCreate(email: $email, password: $password){
+          email
+      }
+  } 
+`;
+
+export const me = gql`
+    query {
+        me{
+            email
+        }
+    }
+`;
