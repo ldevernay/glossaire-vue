3845224af3d5d848d4915f4974f340e20aca7694
Callan <cerveauxallanjean@gmail.com>
2019-01-09 15:30:31 +0100

    create component and add getters & actions to get all users or currentUser

---
+++ src/components/User/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h3>all User</h3>
+    <ul>
+      <li v-for="user in users" >{{user.email}}</li>
+    </ul>
+  </section>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  import {Action, Getter} from 'vuex-class';
+
+  @Component
+  export default class Index extends Vue {
+      @Action("GET_ALL_USERS") allUsers;
+      @Getter("getAllUser") users;
+
+      created(){
+          this.allUsers();
+      }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/User/Show.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>{{user.email}}</h1>
+  </section>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from "vue-property-decorator";
+  import {Action, Getter} from "vuex-class"
+
+  @Component
+  export default class Show extends Vue {
+    @Action("GET_CURRENT_USER") currentUser;
+    @Getter('getCurrentUser') user;
+
+    created(){
+        this.currentUser()
+    }
+  }
+</script>
