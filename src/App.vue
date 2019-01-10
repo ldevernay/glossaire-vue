de1162caf8aa87d1200a01f0ea4704e04fd2722c
Callan <cerveauxallanjean@gmail.com>
2019-01-10 11:56:51 +0100

    remove one link

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
         <router-link to="/user" >Me</router-link> |
         <router-link to="/word">Words</router-link> |
         <router-link to="/word/create">Create Word</router-link> |
-        <router-link to="/word/show/1">Show First Word</router-link> |
         <a href="#" @click.prevent="logout">Disconnect</a>
       </div>
       <router-view/>
